63c3f66f5c4707e4e1352f42712b9db5b00f6e01
ThanePatrol <mandalidis.hugh@gmail.com>
2023-11-13 21:51:44 +1100

    feat: forgot to add

--- editor.js
+++ editor.js
@@ -1,18 +1,22 @@
-import {basicSetup, EditorView} from "codemirror"
-import {EditorState, Compartment} from "@codemirror/state"
-import {python} from "@codemirror/lang-python"
+import {EditorView, keymap} from "@codemirror/view"
+import {defaultKeymap} from "@codemirror/commands"
+import { javascript } from "@codemirror/lang-javascript";
+import { python } from "@codemirror/lang-python";
+import { basicSetup } from 'codemirror';
 
-let language = new Compartment, tabSize = new Compartment
 
-let state = EditorState.create({
-  extensions: [
-    basicSetup,
-    language.of(python()),
-    tabSize.of(EditorState.tabSize.of(8))
-  ]
+let myView = new EditorView({
+	doc: 
+	extensions: [
+		basicSetup,
+		keymap.of(defaultKeymap),
+
+	],
+	parent: document.querySelector("#editor")
 })
+myView.lineWrapping = true;
+myView.dom.style.fontSize = "14px";
+myView.theme = "ambiance";
 
-let view = new EditorView({
-  state,
-  parent: document.body
-})
+
+
